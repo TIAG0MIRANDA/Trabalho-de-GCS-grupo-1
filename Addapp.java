006afe4fd069f95d122e5c33d36a1687450b8b30
bkAlice <alicekoepp@hotmail.com>
2024-10-07 22:36:16 -0300

    Update Addapp.java

--- Addapp.java
+++ Addapp.java
@@ -1,6 +1,16 @@
-System.out.println("[6] Listar proposta");
+System.out.println("[6] Listar propostas feitas");
+System.out.println("[7] Listar propostas recebidas");
+
+
 
 case "6":
-                Clear.clear();
-                usuarioAtual.listarTrocas();
-                break;
+    Clear.clear();
+    usuarioAtual.listarTrocasFeitas();
+    sc.nextLine(); 
+    break;
+
+case "7":
+    Clear.clear();
+    usuarioAtual.listarTrocasRecebidas();
+    sc.nextLine(); 
+    break;
