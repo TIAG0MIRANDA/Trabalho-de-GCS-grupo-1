dc66e839e406b02e841d5a826d3d6a6ad0f4b9a5
bkAlice <alicekoepp@hotmail.com>
2024-10-07 22:38:23 -0300

    Update Adduser.java

--- Adduser.java
+++ Adduser.java
@@ -1,17 +1,33 @@
-private ArrayList<Trade> trocas = new ArrayList<>();
+private ArrayList<Trade> trocasFeitas = new ArrayList<>();    
+private ArrayList<Trade> trocasRecebidas = new ArrayList<>(); 
 
 
-    public void addTroca(Trade troca) {
-        trocas.add(troca);
-    }
+public void addTrocaFeita(Trade troca) {
+    trocasFeitas.add(troca);
+}
 
-    public void listarTrocas() {
-        if (trocas.isEmpty()) {
-            System.out.println("Nenhuma proposta de troca disponível.");
-        } else {
-            for (Trade troca : trocas) {
-                System.out.println(troca.verProposta());
-            }
+
+public void addTrocaRecebida(Trade troca) {
+    trocasRecebidas.add(troca);
+}
+
+public void listarTrocasFeitas() {
+    if (trocasFeitas.isEmpty()) {
+        System.out.println("Nenhuma proposta de troca feita.");
+    } else {
+        for (Trade troca : trocasFeitas) {
+            System.out.println("Proposta feita: " + troca.verProposta());
         }
     }
-    
+}
+
+
+public void listarTrocasRecebidas() {
+    if (trocasRecebidas.isEmpty()) {
+        System.out.println("Nenhuma proposta de troca recebida.");
+    } else {
+        for (Trade troca : trocasRecebidas) {
+            System.out.println("Proposta recebida: " + troca.verProposta());
+        }
+    }
+}
